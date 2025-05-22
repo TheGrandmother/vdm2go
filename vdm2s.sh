cd4645f2dca23bb7e81525cc377376d7bd2b54ef
Henrik Sommerland <henrik.sommerland@epidemicsounds.com>
2025-05-22 09:25:25 +0200

    bundle jars

--- vdm2s.sh
+++ vdm2s.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -cp '/home/grandmother/.m2/repository/vdmtoolkit/vdm-antlr/1.2.0-SNAPSHOT/vdm-antlr-1.2.0-SNAPSHOT.jar:/home/grandmother/.local/lib/antlr-4.11.1-complete.jar:/home/grandmother/.m2/repository/dk/au/ece/vdmj/vdmj/4.6.0/vdmj-4.6.0.jar' vdmantlr.PrintSTree $@
+java -cp './jars/vdm-antlr-1.2.0-SNAPSHOT.jar:./jars/antlr-4.11.1-complete.jar:./jars/vdmj-4.6.0.jar' vdmantlr.PrintSTree $@
